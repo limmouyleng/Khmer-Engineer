e888341c9dbb511b7a270dd23f742bd3e0f7d24b
mouylenglim <mouyleng@instedd.org>
2015-08-15 22:02:05 +0700

    change list style on sections

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,22 +1,30 @@
 angular.module('xpergineer.controllers', [])
 
     .controller('HomeCtrl', function ($scope, $http) {
-      $http.get('http://localhost:3001/api/articles').then(function(articles) {
-        console.log('Success', articles.data);
+      $http.get('http://192.168.1.4:3000/api/articles').then(function(articles) {
         $scope.items = articles.data
       }, function(err) {
-        console.error('ERR', err);      })
+        console.error('ERR', err);     
+      })
       
       $scope.shouldShowDelete = false;
       $scope.shouldShowReorder = false;
       $scope.listCanSwipe = true;
     })
 
-    .controller('SectionsCtrl', function ($scope, Chats) {
-      $scope.chats = Chats.all();
-      $scope.remove = function (chat) {
-        Chats.remove(chat);
-      };
+    .controller('SectionsCtrl', function ($scope, $http) {
+      $scope.sections = [
+        {name: "Plumbing"},
+        {name: "Fire Protection"},
+        {name: "Electrical"},
+        {name: "Architecture"},
+        {name: "Civil Engineer"},
+        {name: "Mechanical"},
+      ]
+      
+      $scope.shouldShowDelete = false;
+      $scope.shouldShowReorder = false;
+      $scope.listCanSwipe = true;
     })
 
     .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
