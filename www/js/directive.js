8bec78250ffda643c940d248cd24286df02db4e3
mouylenglim <mouyleng@instedd.org>
2015-09-09 17:29:30 +0700

    add button on navigator bar

--- www/js/directive.js
+++ www/js/directive.js
@@ -3,11 +3,15 @@ module.directive('hideTabs', function($rootScope) {
     return {
         restrict: 'A',
         link: function(scope, element, attributes) {
-            scope.$watch(attributes.hideTabs, function(value){
-                $rootScope.hideTabs = value;
+            scope.$on('$ionicView.beforeEnter', function() {
+                scope.$watch(attributes.hideTabs, function(value){
+                    console.log('value : ', value)
+                    $rootScope.hideTabs = value;
+                });
             });
 
             scope.$on('$ionicView.beforeLeave', function() {
+                console.log('hideTabs : ')
                 $rootScope.hideTabs = false;
             });
         }
