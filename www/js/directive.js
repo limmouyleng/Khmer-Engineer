e1e3d0f1b81bb0f76701ab2f1fe3bba57469c1c4
mouylenglim <mouyleng@instedd.org>
2015-09-11 08:55:17 +0700

    hide tab bar in deeper view

--- www/js/directive.js
+++ www/js/directive.js
@@ -5,14 +5,8 @@ module.directive('hideTabs', function($rootScope) {
         link: function(scope, element, attributes) {
             scope.$on('$ionicView.beforeEnter', function() {
                 scope.$watch(attributes.hideTabs, function(value){
-                    console.log('value : ', value)
                     $rootScope.hideTabs = value;
                 });
-            });
-
-            scope.$on('$ionicView.beforeLeave', function() {
-                console.log('hideTabs : ')
-                $rootScope.hideTabs = false;
             });
         }
     };
